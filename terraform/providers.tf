84ac0ae02f7f65a5501bcafb4bc8f7152bf2947e
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-07-16 21:02:13 +0100

    Add initial terraform

---
+++ terraform/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.6.2"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.111.0"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+
+  features {
+    resource_group {
+      # Resource group is only used by workload, App Insights creates artifacts that need to be deleted
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
