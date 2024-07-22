1830bec5ebed306d6855dce901175dea38a4b961
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-07-22 16:33:12 +0100

    Fix expression

--- scripts/configure_bot_environment.ps1
+++ scripts/configure_bot_environment.ps1
@@ -6,6 +6,6 @@ Write-Host "Configuring '$($environment)' bot environment on $($env:COMPUTERNAME
 
 $installDirectory = "C:\bots\$environment"
 
-if (Test-Path -Path $installDirectory -not $true) {
+if (Test-Path -Path $installDirectory -ne $true) {
     New-Item -Path $installDirectory -ItemType Directory -Verbose
 }
