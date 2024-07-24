ee1ca50b43b6d64061c6e87b48cc8eaccf25c351
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-07-24 20:49:25 +0100

    Fleshing out the script

--- scripts/configure_bot_environment.ps1
+++ scripts/configure_bot_environment.ps1
@@ -24,12 +24,23 @@ Write-Host "Config.Environment: $($config.environment)"
 Write-Host "Config.Location: $($config.location)"
 Write-Host "Config.Instance: $($config.instance)"
 
-# Generate access token for the bot to access the repository api
-$accessToken = Get-AccessToken -clientId $client_app_id -clientSecret $client_app_secret -scope "$($config.repository_api.application_audience)/.default"
-
 # Create install directory
 $installDirectory = "C:\bots\$environment"
 
 if ((Test-Path -Path $installDirectory) -ne $true) {
     New-Item -Path $installDirectory -ItemType Directory -Verbose
+}
+
+# Generate access token for the bot to access the repository api
+$accessToken = Get-AccessToken -clientId $client_app_id -clientSecret $client_app_secret -scope "$($config.repository_api.application_audience)/.default"
+
+# Get the game servers that are bot enabled
+$uri = "https://$($config.api_management_name).azure-api.net/$($config.repository_api.apim_path_prefix)/game-servers/"
+$servers = Get-BotEnabledServers -uri $uri -accessToken $accessToken -subscriptionKey $repository_subscription_key
+
+# Loop through the servers and configure the bot
+$servers | ForEach-Object {
+    $server = $_
+
+    Write-Host "Configuring bot for server: $($server.title)"
 }
---
+++ scripts/functions/Get-BotEnabledServers.ps1
@@ -0,0 +1,32 @@
+function Get-BotEnabledServers {
+    [CmdletBinding()]
+    param (
+        [string]$uri,
+        [string]$accessToken,
+        [string]$subscriptionKey
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        $params = @{
+            Uri            = $uri + "?filter=BotEnabled&takeEntries=50&skipEntries=0"
+            Authentication = "Bearer"
+            Token          = $accessToken | ConvertTo-SecureString -AsPlainText -Force
+            Headers        = @{
+                "Ocp-Apim-Subscription-Key" = $subscriptionKey
+            }
+        }
+
+        $response = Invoke-RestMethod @Params
+        $servers = $response.result.entries
+
+        return $servers
+    }
+    
+    end {
+        
+    }
+}
