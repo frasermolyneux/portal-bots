2bdb074484c98b59f64024e152339473137829c3
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-07-26 17:33:41 +0100

    idk

--- scripts/configure_bot_environment.ps1
+++ scripts/configure_bot_environment.ps1
@@ -49,7 +49,7 @@ $servers | ForEach-Object {
     Write-Host "Configuring bot for server: $($server.title)"
 
     $action = New-ScheduledTaskAction -Execute "$installDirectory\b3.exe" -Argument "-c $installDirectory\conf\$($server.gameServerId).ini"
-    $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5) -RepetitionDuration ([System.TimeSpan]::MaxValue)
+    $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5)
     $principal = New-ScheduledTaskPrincipal -UserId "LOCALSERVICE" -LogonType ServiceAccount
     $settings = New-ScheduledTaskSettingsSet
     $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
