2a0cccefd4b9f5cbc4c599ff89af3dcf5c8244fa
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-08-03 20:25:35 +0100

    Update name

--- terraform/key_vault_secrets.tf
+++ terraform/key_vault_secrets.tf
@@ -1,5 +1,5 @@
 resource "azurerm_key_vault_secret" "mysql_connection_string" {
-  name         = "mysql_connection_string"
+  name         = "mysql-connection-string"
   value        = "placeholder"
   key_vault_id = azurerm_key_vault.kv.id
 
